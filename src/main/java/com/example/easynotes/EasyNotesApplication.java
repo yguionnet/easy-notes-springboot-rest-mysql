c47d845fa3d71be630b6cfa5438f41fa7355740c
yguionnet <yannick.guionnet@guiodesv.fr>
2021-12-02 11:36:16 +0100

    first commit

---
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.easynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotesApplication.class, args);
+	}
+
+}
