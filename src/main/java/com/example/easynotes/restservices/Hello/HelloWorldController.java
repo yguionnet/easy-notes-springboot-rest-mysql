4444afbb1a8a2b4109afeb65c8d532a9a03fcfc0
yguionnet <yannick.guionnet@guiodesv.fr>
2021-12-02 15:20:31 +0100

    Initialisation completed - second commit

---
+++ src/main/java/com/example/easynotes/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.easynotes.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	//Simple Method
+		//URI - /helloworld
+		//GET 
+		//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+		@GetMapping("/helloworld1")
+		public String helloWorld() {
+			return "Hello World1";
+		}
+		
+		@GetMapping("/helloworld-bean")
+		public UserDetails helloWorldBean() {
+			return new UserDetails("Yannick", "Guionnet", "Paris");
+		}
+}
